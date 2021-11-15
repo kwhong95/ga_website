716fa54da29fe4babfcb17b782c4cae9ed185ff3
kwhong95 <kwhong95@kakao.com>
2021-11-15 21:31:35 +0900

    INITIAL React GA

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import ReactGa from "react-ga";
+
 export default function App() {
+  useEffect(() => {
+    ReactGa.initialize("G-F85BG5VQJ0");
+
+    ReactGa.pageview("/");
+  }, []);
+
   return <h1>Hello~</h1>;
 }
